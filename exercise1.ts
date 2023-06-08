de6dc94fdff1dffe2fd06f7d41c044c36dae32c9
nguyen quoc toan <toanprokun77@gmail.com>
2023-06-08 11:01:25 +0700

    sửa lại bài 1 ạ

--- exercise1.ts
+++ exercise1.ts
@@ -1,10 +1,11 @@
 //hàm getfullname
+
 function getFullName(firsName: String, lastName:String): void{
     //in ra
     console.log(`full Name: ${firsName} ${lastName}`);
 }
 
-//hàm getfullname được gán cho biến fullname 
+// Sử dụng hàm để nhập tên 
 const firsName: string = prompt(`nhập họ: `)!;
 const lastName: string = prompt(`nhập tên : `)!;
 getFullName(firsName, lastName);
